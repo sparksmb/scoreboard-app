9cfc95dece8d7f38f766e9dec56131138730f061
Michael Sparks <sparksmb@gmail.com>
2025-08-20 20:51:50 -0500

    Convert timeouts from number to graphic squares.

--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -1,23 +1,27 @@
 class Scoreboard < ApplicationRecord
   belongs_to :game
-  
+
   validates :type, presence: true
   validates :home_score, :visitor_score, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :home_timeouts_remaining, :visitor_timeouts_remaining, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 3 }
-  
+
   after_update :broadcast_update
-  
+
   # STI types
   def self.types
     %w[FootballScoreboard BasketballScoreboard SoccerScoreboard]
   end
-  
+
   def sport_name
     type.gsub('Scoreboard', '').downcase
   end
-  
+
+  def timeout_image_path(timeouts_remaining)
+    "/images/timeouts_#{timeouts_remaining}.svg"
+  end
+
   private
-  
+
   def broadcast_update
     ActionCable.server.broadcast(
       "scoreboard_#{game.organization_id}",
